b1f7c163386d31691cccca8df97c7ceb309d161a
OlapadaSeca <bruno.98902@aluno.pr.senac.br>
2024-11-26 21:44:54 -0300

    Upando para o git

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 import { DataUser } from "./DataUser.js";
 
 const dataUser = new DataUser();
-document.getElementById("botaoEnviar").addEventListener("click", cadastrarUsuario);
-document.getElementById("papaiNoelImg").addEventListener("click", santaClausLaughingOnClick);
+document
+  .getElementById("botaoEnviar")
+  .addEventListener("click", cadastrarUsuario);
+document
+  .getElementById("papaiNoelImg")
+  .addEventListener("click", santaClausLaughingOnClick);
 document.addEventListener("DOMContentLoaded", jingleBells);
+
 /**
  * Função que cadastra um usuário no sistema.
  * @param {Event} e - Evento que ocorre quando o formulário é submetido.
@@ -17,10 +22,17 @@ function cadastrarUsuario(e) {
   const tipobrinquedo = document.getElementById("tipobrinquedo").value;
   const genero = document.getElementById("genero").value;
   const telefone = Number(document.getElementById("telefone").value);
-  if(nome == "" || email == "" || comentarios == "" || tipobrinquedo == "" || genero == "" || telefone == "" ){
-    alert("Preencha todos os campos")
-    return sadNoel()
-  }else{
+  if (
+    nome == "" ||
+    email == "" ||
+    comentarios == "" ||
+    tipobrinquedo == "" ||
+    genero == "" ||
+    telefone == ""
+  ) {
+    alert("Preencha todos os campos");
+    return sadNoel();
+  } else {
     dataUser.cadastrarUsuario(
       nome,
       email,
@@ -30,41 +42,39 @@ function cadastrarUsuario(e) {
       telefone
     );
     console.log(dataUser);
-    
+
     alert("Usuário cadastrado com sucesso!");
-    return santaClausLaughingOnSend()
+    return santaClausLaughingOnSend();
   }
- 
 }
-function santaClausLaughingOnSend(){
-  const audio = document.createElement("audio")
-  const papaiNoelImg = document.getElementById("papaiNoelImg")
-  papaiNoelImg.src="./assets/papaiNoel.jpg"
-  audio.src = "./audio/santaClaus.mp3"
-  document.body.appendChild(audio)
-  audio.play()
+function santaClausLaughingOnSend() {
+  const audio = document.createElement("audio");
+  const papaiNoelImg = document.getElementById("papaiNoelImg");
+  papaiNoelImg.src = "./assets/papaiNoel.jpg";
+  audio.src = "./audio/santaClaus.mp3";
+  document.body.appendChild(audio);
+  audio.play();
 }
-function santaClausLaughingOnClick(){
+function santaClausLaughingOnClick() {
+  const audio = document.createElement("audio");
+  audio.src = "./audio/santaClausLaughing.mp3";
+  document.body.appendChild(audio);
+  audio.play();
+}
+function sadNoel() {
+  const audio = document.createElement("audio");
+  audio.src = "./audio/clash-royale-king-cry.mp3";
+  document.body.appendChild(audio);
+  audio.play();
+  const imagemNoelTriste = document.getElementById("papaiNoelImg");
+  imagemNoelTriste.src =
+    "./assets/papaiTristePorqueVoceEBurroEerrouOformulario.jpg";
+}
+function jingleBells() {
+  const audio = document.createElement("audio");
+  audio.src = "./audio/jingle-bells-180104.mp3";
+  document.body.appendChild(audio);
+  audio.play();
 
- const audio = document.createElement("audio")
- audio.src = "./audio/santaClausLaughing.mp3"
- document.body.appendChild(audio)
- audio.play()
-
-}
-function sadNoel(){
-  const audio = document.createElement("audio")
-  audio.src = "./audio/clash-royale-king-cry.mp3"
-  document.body.appendChild(audio)
-  audio.play()
-  const imagemNoelTriste = document.getElementById("papaiNoelImg")
-  imagemNoelTriste.src = "./assets/papaiTristePorqueVoceEBurroEerrouOformulario.jpg"
-}
-function jingleBells(){
-  const audio = document.createElement("audio")
-  audio.src = "./audio/jingle-bells-180104.mp3"
-  document.body.appendChild(audio)
-  audio.play() 
-  audio.loop = true
-  audio.volume = 0.1
+  audio.volume = 0.1;
 }
